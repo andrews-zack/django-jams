62851e6799f9b4e0f205d1c6ce899a6f180b75aa
andrews-zack <zachary.andrews.au19@gmail.com>
2022-11-16 20:44:26 +0000

    creating dev branch and saving unique fields

---
+++ api/djams/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class Artist(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    listeners = models.IntegerField(null=False, blank=False)
+    biography = models.TextField()          # Look up params
+    image = models.URLField()           # Look up params
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    length = models.FloatField(null=False, blank=False)
+    explicit = models.BooleanField(null=False, blank=False)         # Look up params
+    plays = models.IntegerField(null=False, blank=False)
+    album = models.ForeignKey("Album", on_delete=models.PROTECT)
+    genre = models.ForeignKey("Genre", on_delete=models.PROTECT)
+    created_at = models.DateTimeField()         # Look up params
+    updated_at = models.DateTimeField()         # Look up params
+
+class Album(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    year_released = models.DateField(null=False, blank=False)     # Look up params
+    is_original = models.BooleanField(null=False, blank=False)
+    artist = models.ForeignKey("Artist", on_delete=models.PROTECT)
+    created_at = models.DateTimeField()         # Look up params
+    updated_at = models.DateTimeField()
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    created_at = models.DateTimeField()         # Look up params
+    updated_at = models.DateTimeField()
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    created_at = models.DateTimeField()         # Look up params
+    updated_at = models.DateTimeField()
+
+class Keyword(models.Model):
+    title = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField()         # Look up params
+    updated_at = models.DateTimeField()
+
